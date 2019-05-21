7c8b5f96112161e64ddebb459a51bc79be2b1cef
Aleksey Belchenkov <u608110@gmail.com>
2019-05-21 18:01:51 +0300

    Module & Optimaze Bundle

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Webpack');
+import { config } from './modules/config';
+import AppService from './modules/app.service';
+import './modules/header.component';
+
+console.log('From Config', config.key);
+
+const service = new AppService('Hello');
+service.log();
