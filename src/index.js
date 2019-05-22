e11c59f68acc2223db2f583d64fa1a9a0aed3616
Aleksey Belchenkov <u608110@gmail.com>
2019-05-22 14:55:34 +0300

    Compile LESS

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import { config } from './modules/config';
 import AppService from './modules/app.service';
 import './modules/header.component';
 import './css/index.css';
+import './less/index.less';
