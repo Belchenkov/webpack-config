1a952ad569f588516077e23aab0367085d5a7c89
Aleksey Belchenkov <u608110@gmail.com>
2019-05-22 16:22:23 +0300

    Compile TypeScript

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import {render} from 'react-dom';
+
 import App from './App';
 import { config } from './modules/config';
 import AppService from './modules/app.service';
+
+import './modules/ts.module';
 import './css/index.css';
 import './less/index.less';
 import './scss/index.scss';
